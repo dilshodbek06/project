d1a7e014471b5fa3fbcb7ed3a3c717ccca3be477
Ziyodullayev Dilshodbek <ziyodullayevdilshod7@gmail.com>
2023-09-17 12:40:58 +0500

    create blank project

---
+++ src/redux/slices/RootReducer.js
@@ -0,0 +1,8 @@
+import {combineReducers} from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({
+    // Add more reducers here if needed
+    // login: LoginReducer,
+});
+
+export default rootReducer;
